575029d72205f17d24a903e92f15ce11c0bce9cb
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-08 00:30:30 -0600

    [add] wouter and add a new page route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
 import giffyLogo from '/logo.svg'
+import Home from '../pages/Home'
 import { ListGifs } from './ListGifs'
 import '../App.css'
+
+import {Link, Route} from 'wouter'
 
 function App(){
   return (
     <>
       {/* Header */}
       <header className='header'>
-        <figure className='logo'>
-          <img src={giffyLogo} alt="Logo Giffy" />
-          <figcaption>Giffy</figcaption>
-        </figure>
+        <Link to="/">
+          <figure className='logo'>
+            <img src={giffyLogo} alt="Logo Giffy" />
+            <figcaption>Giffy</figcaption>
+          </figure>
+        </Link>
       </header>
 
       {/* List of gifs */}
+
+      <Route
+        component={Home}
+        path="/"/>
+
       <main>
         <div className='gifs__container'>
           <ListGifs keyword='Selena Gomez'/>
