c56800d9bfdbe59822e8e3243345e8ef6b4419e4
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-09 21:35:58 -0600

    [add] the searcher structure and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,47 @@
 import { Link } from 'wouter';
+import { SearchIcon } from '../../components/icons/Search';
 import './home.css'
 
 const POPULAR_GIFS = ['Selena Gomez', 'Batman', 'Spiderman']
 
 export default function Home(){
   return (
-    <main className='home__container'>
-      <h2 className='popular__title'>The most popular Gifs 🖼️</h2>
-      <ul className='popular__list'>
-        {
-          POPULAR_GIFS.map(gif => (
-            <li key={gif}>
-              <Link to={`search/${gif}`}>
-                {gif} gifs
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-    </main>
+    <div className='home__container'>
+      {/* Searcher */}
+      <div className='searcher__wrapper'>
+          <form action="" className='searcher__form'>
+            <div className='searcher'>
+              <span>
+                <SearchIcon/>
+              </span>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </form>
+      </div>
+
+      {/* Gifs */}
+      <main className='last-gifs'>
+        <h2>Your last search</h2>
+      </main>
+
+      {/* Trending categories */}
+      <aside className='popular__gifs'>
+        <h3 className='popular__title'>
+          The most popular Gifs 🖼️
+        </h3>
+        <ul className='popular__list'>
+          {
+            POPULAR_GIFS.map(gif => (
+              <li key={gif}>
+                <Link to={`search/${gif}`}>
+                  {gif} gifs
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </aside>
+    </div>
   )
 }
