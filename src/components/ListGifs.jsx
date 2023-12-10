a2fb8b084bf64210d66fc0a6d70ac682b8328ab7
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-09 21:58:44 -0600

    [create] custom hook useGifs

--- src/components/ListGifs.jsx
+++ src/components/ListGifs.jsx
@@ -1,32 +1,21 @@
-import { useState, useEffect } from "react";
 import Gif from "./Gif";
-import getGifs from "../services/getGifs";
 import Spin from "./Spin";
+import {useGifs} from '../hooks/useGifs.js';
 
-export function ListGifs({keyword = 'cruz azul'} = {}){
-  const [loading, setLoading] = useState(false)
-  const [gifs, setGifs] = useState([]);
+export function ListGifs({keyword = 'random'} = {}){
+  const {loading, gifs} = useGifs({keyword})
 
-  useEffect(() => {
-    setLoading(true)
-    getGifs(keyword)
-      .then(gifs => {
-        setGifs(gifs)
-        setTimeout(() => {
-          setLoading(false)
-        }, 800)
-      })
-  }, [keyword])
-
-  if(loading) return <Spin/>
-
-  return <>
-    {
-      !loading && gifs.map(({title, id, url}) => {
-        return <Gif key={id}
-        title={title}
-        url={url}/>
-      })
-    }
-  </>
+  return (
+    <>
+      {
+        loading
+          ? <Spin/>
+          : gifs.map(({title, id, url}) => {
+          return <Gif key={id}
+          title={title}
+          url={url}/>
+        })
+      }
+    </>
+  )
 }
