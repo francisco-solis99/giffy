01d1244d7b3e933b2f91cd0efb9a4f57acafa4b8
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-19 22:27:48 -0600

    [add] the useGifs hokk in the details page to get the gif selected

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,8 +1,9 @@
 import Gif from "../../components/Gif"
-import { useGifContext } from "../../context/GifContext"
+// import { useGifContext } from "../../context/GifContext"
+import { useGifs } from "../../hooks/useGifs"
 
 export default function Detail({ params }) {
-  const { gifs } = useGifContext()
+  const { gifs } = useGifs()
 
   const gifSelected = gifs.find(singleGif => singleGif.id === params.id)
 
