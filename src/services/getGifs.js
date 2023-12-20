ced6a22341c0802c19073505bf3d6785ddf86b13
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-20 02:17:48 -0600

    [refactor] getGifs services

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,19 +1,26 @@
-import {API_KEY} from './setings'
+import { API_KEY, API_URL } from './setings'
 
-export default function getGifs({keyword}){
 
-  const urlAPI = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
-
-  return fetch(urlAPI)
-  .then(response => response.json())
-  .then(({data}) => {
+const fromApiResponseToGifs = (apiResponse) => {
+  const { data = [] } = apiResponse
+  if(Array.isArray(data)) {
     const dataGifs = data.map(item => {
-      const {id, title, images} = item;
-      const {url} = images.fixed_height;
+      const { id, title, images } = item;
+      const { url } = images.fixed_height;
       return {
         id, title, url
       }
     })
     return dataGifs
-  })
+  }
+  return [];
+}
+
+export default function getGifs({ keyword }) {
+
+  const urlAPI = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
+  return fetch(urlAPI)
+    .then(response => response.json())
+    .then(fromApiResponseToGifs)
 }
