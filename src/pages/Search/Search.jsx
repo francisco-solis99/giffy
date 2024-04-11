d0014deffccaaa509bf5f23c8df5da66ad727e24
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-11 14:22:19 -0600

    [chore] add the list of gifs searched in search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,16 +1,17 @@
 import { ListGifs } from '../../components/ListGifs'
+import { useGifs } from '../../hooks/useGifs';
 import './search.css'
 
 
 export default function Search({ params }) {
   const { keyword } = params;
-
+  const {gifs, loading} = useGifs({keyword})
   return (
     <>
       <main>
         <h2 className='search__title'>{decodeURI(keyword)}</h2>
         <section className='gifs__container'>
-          <ListGifs keyword={keyword}/>
+          <ListGifs gifs={gifs} loading={loading}/>
         </section>
       </main>
     </>
