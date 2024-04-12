1d7179e6e62d2cf816cb11348a2fcb05927f6c7c
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-12 13:37:16 -0600

    [chore] add pagination state to pass

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,26 +1,43 @@
 import { useEffect } from 'react';
 import getGifs from '@services/getGifs';
 import { useGifContext } from '@context/GifContext';
+import { useState } from 'react';
 
 
-
-export function useGifs({keyword} = {}){
+const INITIAL_PAGE = 0;
+export function useGifs({keyword = null} = {}) {
+  const [page, setPage] = useState(INITIAL_PAGE)
+  const [loadingNextGifs, setLoadingNextGifs] = useState(false)
   const {loading, setLoading, gifs, setGifs} = useGifContext()
+
+  // Use local storage to get the last search to get the key or the lastkeyword or random
+  const keywordToUse = keyword || window.localStorage.getItem('lastKeyword');
 
   useEffect(() => {
     setLoading(true)
-    // Use local storage to get the last search to get the key or the lastkeyword or random
-    const keywordToUse = keyword || window.localStorage.getItem('lastKeyword');
-    getGifs({keyword: keywordToUse === 'undefined' ? 'random' : keywordToUse})
+    getGifs({keyword: keywordToUse})
       .then(gifs => {
         setGifs(gifs)
         setTimeout(() => {
           setLoading(false)
         }, 800)
-        // Save the last keyword
-        window.localStorage.setItem('lastKeyword', keyword)
+        // Save the last keyword if is passed
+        if(keyword) window.localStorage.setItem('lastKeyword', keyword)
       })
-  }, [keyword, setGifs, setLoading])
+  }, [keyword, setGifs, setLoading, keywordToUse])
 
-  return {loading, gifs}
+  useEffect(() => {
+    if(INITIAL_PAGE === page) return;
+
+    setLoadingNextGifs(true)
+    getGifs({keyword: keywordToUse, page})
+      .then(nextGifs => {
+        setGifs(prevGifs => [...prevGifs, ...nextGifs])
+        setTimeout(() => {
+          setLoadingNextGifs(false)
+        }, 800)
+      })
+  }, [keywordToUse, setGifs, page])
+
+  return {loading, gifs, loadingNextGifs, setPage}
 }
