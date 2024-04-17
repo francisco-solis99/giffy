2154344d5a522fba40c9ca433c3f02a469643bc3
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-17 17:57:33 -0600

    [pef] add memoization to avoid re-render of all the gifs

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import { Link } from "wouter";
 
-export default function Gif(props) {
+function Gif(props) {
   const { url, title, id } = props;
   return <article className="gif">
     <Link to={`/gif/${id}`}>
-      <figure>
+      <figure className="gif__link">
         <img src={url} alt={title} />
         <figcaption>{title}</figcaption>
       </figure>
     </Link>
   </article>
 }
+
+export default React.memo(Gif, (prevProps, nextProps) => {
+  return prevProps.id === nextProps.id
+})
