f101bc58e326e41b3b3cac08e35714dbebc914ae
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-25 13:36:29 -0600

    [feat] add the login page with the login action in the user context

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,44 @@
+import { useState } from "react"
+import { useLocation } from "wouter"
+import { useUserContext } from "@context/UserContext"
+import { useEffect } from "react"
 
 export default function Login() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [, navigate] = useLocation()
+  const { login, isLogged} = useUserContext()
+
+  useEffect(() => {
+    if(isLogged) navigate('/')
+  }, [isLogged, navigate])
+
+  const handleChangeUserName = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmitLogin = (e) => {
+    e.preventDefault()
+    login()
+  }
+
   return (
-    <div>Login</div>
+    <form className="login__form" onSubmit={handleSubmitLogin}>
+      <label htmlFor="username">
+        <span>Username</span>
+        <input type="text" name="username" value={userName} onChange={handleChangeUserName} required />
+      </label>
+      <label htmlFor="password">
+        <span>Password</span>
+        <input type="password" name="password" value={password} onChange={handleChangePassword} required />
+      </label>
+      <button type="submit">
+        Login
+      </button>
+    </form>
   )
 }
