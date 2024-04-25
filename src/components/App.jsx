c7ad739c177c3b61bc1398ac9d6866c19818bb5d
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-24 22:54:16 -0600

    [chore] add login route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
-import giffyLogo from '/logo.svg'
+import { Route} from 'wouter'
+
 import Home from '@pages/Home/Home'
 import Search from '@pages/Search/Search'
 import Detail from '@pages/Detail/Detail'
+import Login from '@pages/Login/Login'
+import Header from '@components/Header'
+
 import { GifProvider } from '@context/GifContext'
+
 import '../App.css'
 
-import {Link, Route} from 'wouter'
 
 function App(){
+
+
+
   return (
     <>
       {/* Header */}
-      <header className='header'>
-        <Link to="/">
-          <figure className='logo'>
-            <img src={giffyLogo} alt="Logo Giffy" />
-            <figcaption>Giffy</figcaption>
-          </figure>
-        </Link>
-      </header>
+      <Header/>
 
       {/* List of gifs */}
       <GifProvider>
@@ -35,6 +35,10 @@ function App(){
           path="/gif/:id"/>
 
         <Route
+          component={Login}
+          path="/login"/>
+
+        <Route
           component={() => <h1>404 Error ⚠️</h1>}
           path="/404"/>
       </GifProvider>
@@ -43,3 +47,24 @@ function App(){
 }
 
 export default App;
+
+/*
+TODOS:
+[✅] Add the header nav component
+[✅] Add login route
+[] Add login page
+[] create the user context
+[] create custom hook useUser
+[] use the useUser hook in Login page
+[] create the logout
+[] connect login to api
+[] Add loading and error state to login page
+[] Create the favorite actions component
+[] Save jwt ins sessionStorage
+[] Test favorite and unfavorite
+[] Add favorites section to home page
+
+
+
+
+*/
