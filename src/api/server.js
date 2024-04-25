09fad6edb638f14c34a657a3fa49f406b952c815
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-25 14:22:12 -0600

    [chore] use cors in the init server file

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import { PORT } from './config.js'
 import favoritesRoutes from './routes/favorites.routes.js'
 import login from './routes/login.js'
@@ -7,6 +8,7 @@ import login from './routes/login.js'
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(login)
